080a8ab850e76360bee2b599e8039f076dc4eab1
Beth-H-coder <bethhughesis@hotmail.com>
2022-07-15 14:34:21 +0100

    updte the app to-do list

--- src/App.js
+++ src/App.js
@@ -1,57 +1,77 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-  const [toDos, setToDos] = useState([
-    "coding", "reading"
-  ]);
-  
+  const [toDos, setToDos] = useState(["coding", "reading"]);
+  console.log(toDos);
   return (
     <>
-    <Header />
-    <Adder toDos={toDos} setToDos={setToDos}/>
-    <ToDoList toDos={toDos} />
+      <Header />
+      <Adder toDos={toDos} setToDos={setToDos} />
+      <ToDoList toDos={toDos} setToDos={setToDos} />
     </>
   );
-};
-
+}
 
 const Adder = (props) => {
-  const [newToDo, setNewToDo] = useState("") 
-  const {toDos, setToDos} = props;
+  const [newToDo, setNewToDo] = useState("");
+  const { toDos, setToDos } = props;
 
-  const submitHandler = (e) => {e.preventDefault()
-      setToDos([...toDos, newToDo])
-      console.log(newToDo, "<< ON CLICK")}
-  
-  console.log(toDos, "<< ADDED!")
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setToDos([...toDos, newToDo]);
+    console.log(newToDo, "<< ON CLICK");
+    setNewToDo("");
+  };
+
+  console.log(toDos, "<< ADDED!");
 
   return (
     <>
-    <form>
-      <label for="todo">Add a new TODO</label>
-      <input type="text" name="todo" id="todo" value={newToDo} onChange={(e) => setNewToDo(e.target.value)}></input>
-      <button onClick={submitHandler} type="submit">Send</button>
-    </form>
+      <form>
+        <label for="todo">Add a new TODO</label>
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          value={newToDo}
+          onChange={(e) => setNewToDo(e.target.value)}
+        ></input>
+        <button onClick={submitHandler} type="submit">
+          Send
+        </button>
+      </form>
     </>
   );
 };
 
 const Header = () => {
-      return <h1>Alex's To Do List</h1>
-  };
+  return <h1>Alex's To Do List</h1>;
+};
 
-  const ToDoList = (props) => {
-    return (
-    <div>
-    <ul>
-      {props.toDos.map((toDo) => {
-        return <li key={toDo}>{toDo}</li>
-      })}
-    </ul>
-    </div>
+const ToDoList = (props) => {
+  console.log(props);
+  const deleteItem = (index) => () => {
+    props.setToDos((toDos) =>
+      toDos.filter((_, i) => {
+        return i !== index;
+      })
     );
   };
 
-
+  return (
+    <div>
+      <ul>
+        {props.toDos.map((toDo, index) => {
+          return (
+            <li key={toDo}>
+              {toDo}
+              <button onClick={deleteItem(index)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
